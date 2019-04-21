5c8630098ac130d43844b2ab70a899ac8c24fff3
btkramm <btkramm@uc.cl>
2019-04-21 18:22:26 -0400

    Close

--- README.md
+++ README.md
@@ -21,3 +21,7 @@ firebase login
 ```bash
 firebase deploy
 ```
+
+https://firebase.google.com/docs/hosting/deploying?hl=es-419
+https://console.firebase.google.com/u/0/project/pwa-twitter-4ddd4/hosting/main?hl=es-419&pli=1
+https://firebase.google.com/docs/hosting/
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 
 module.exports = {
   mode: 'development',
-
-  devtool: 'inline-source-map',
 
   entry: {
     bundle: path.resolve(__dirname, 'src', 'index.js'),
@@ -11,38 +11,80 @@ module.exports = {
 
   output: {
     path: path.resolve(__dirname, 'build'),
-    publicPath: 'http://localhost:8080/',
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+    new WebpackPwaManifest({
+      name: 'My Progressive Web App',
+      short_name: 'MyPWA',
+      description: 'My awesome Progressive Web App!',
+      background_color: '#ffffff',
+      crossorigin: 'use-credentials',
+      icons: [
+        {
+          src: path.resolve(
+            __dirname,
+            'src',
+            'assets',
+            'images',
+            'logo-48.png'
+          ),
+          sizes: [96, 128, 192, 256, 384, 512],
+        },
+      ],
+    }),
+  ],
+
+  devtool: 'inline-source-map',
 
   devServer: {
-    contentBase: path.join(__dirname, 'src'),
+    contentBase: path.join(__dirname, 'public'),
     headers: { 'Access-Control-Allow-Origin': 'http://localhost:8080' },
     port: 8080,
-  },
-
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src/'),
-    },
   },
 
   module: {
     rules: [
       {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(gif|jpg|png|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: 'assets/images',
+            },
+          },
+        ],
       },
 
       {
         test: /\.sass$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
-
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
     ],
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/'),
+    },
   },
 };
