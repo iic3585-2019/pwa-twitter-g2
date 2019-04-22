14f638a240fdd1d2704a19e5f7744976ccac07e9
btkramm <btkramm@uc.cl>
2019-04-22 11:29:53 -0400

    Observables

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
+import { Observable, fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
 import runtime from 'serviceworker-webpack-plugin/lib/runtime';
-import { fromEvent } from 'rxjs';
+import '@/assets/stylesheets/index.sass';
 
 // Firebase setup
 import firebase from 'firebase/app';
-import 'firebase/database';
-
-// Stylesheets
-import '@/assets/stylesheets/index.sass';
+import 'firebase/firestore';
 
 const config = {
-  apiKey: "AIzaSyDPyYczw1sWppmj_MNqQAyROTgydtd1aN4",
-  authDomain: "pwa-twitter-4ddd4.firebaseapp.com",
-  databaseURL: "https://pwa-twitter-4ddd4.firebaseio.com",
-  projectId: "pwa-twitter-4ddd4",
-  storageBucket: "pwa-twitter-4ddd4.appspot.com",
-  messagingSenderId: "825325030164"
+  apiKey: 'AIzaSyDPyYczw1sWppmj_MNqQAyROTgydtd1aN4',
+  authDomain: 'pwa-twitter-4ddd4.firebaseapp.com',
+  databaseURL: 'https://pwa-twitter-4ddd4.firebaseio.com',
+  projectId: 'pwa-twitter-4ddd4',
+  storageBucket: 'pwa-twitter-4ddd4.appspot.com',
+  messagingSenderId: '825325030164',
 };
 firebase.initializeApp(config);
 
@@ -23,26 +22,52 @@ if ('serviceWorker' in navigator) {
   const registration = runtime.register();
 }
 
-console.log("aca se muestra toda la database");
-const database = firebase.database();
+const db = firebase.firestore();
 
-const ref = database.ref('tweets');
+const snapshots$ = Observable.create(observer =>
+  db
+    .collection('tweets')
+    .orderBy('created_at')
+    .onSnapshot(observer)
+);
 
-console.log("los tweets");
+const tweets$ = snapshots$.pipe(
+  map(snapshot => snapshot.docs.map(s => s.data()))
+);
 
-console.log(ref);
+tweets$.subscribe(drawTweets);
 
-const getKeyCode = (event) => event.keycode || event.which;
-//Create Tweet
-fromEvent(document, 'keyup')
-  .subscribe((event) => {
-    if (getKeyCode(event) === 13){
-      const text = document.querySelector('input').value;
-      const data = {
-        body: text,
-        likes: 0
-      };
-      ref.push(data);
-      console.log(text);
-    }
+const drawTweets = tweets => {
+  // vaciar timeline
+  // for tweet in tweet, dibujar tweet dentro de timeline
+};
+
+const pushComment = (tweet_id, body) => {
+  return db.collection('comments').add({ tweet_id: '1', body });
+};
+
+const pushTweet = body => {
+  return ref.push({ body, likes });
+};
+
+// Create Tweet
+const getKeyCode = event => event.keycode || event.which;
+
+fromEvent(document, 'keyup').subscribe(event => {
+  if (getKeyCode(event) === 13) {
+    const body = document.querySelector('input').value;
+
+    pushTweet(body);
+  }
+});
+
+// Comment Tweet
+const button = document.querySelector('.comment-btn');
+
+fromEvent(button, 'click').subscribe(() => {
+  const body = prompt('Cuerpo del comentario');
+
+  pushComment('hola', body).then(a => {
+    console.log(a);
   });
+});
