7d8e23958b48f7ca7fac5a196873ef70c0611b1a
Rodrigo Cortes <racortes2@uc.cl>
2019-04-21 22:57:04 -0400

    print text in input

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import runtime from 'serviceworker-webpack-plugin/lib/runtime';
 
+import { fromEvent } from 'rxjs';
+
 // Stylesheets
 import '@/assets/stylesheets/index.sass';
 
 if ('serviceWorker' in navigator) {
   const registration = runtime.register();
 }
+
+const getKeyCode = (event) => event.keycode || event.which;
+//Create Tweet
+fromEvent(document, 'keyup')
+  .subscribe((event) => {
+    if (getKeyCode(event) === 13){
+      const text = document.querySelector('input').value;
+      console.log(text);
+    }
+  });
+
