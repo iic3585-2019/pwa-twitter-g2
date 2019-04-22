48a3d06ff57a3e9682c2f8dce8c193d578352851
Benjamín Thomas Kramm Pino <btkramm@uc.cl>
2019-04-21 21:03:17 -0400

    Firebase (#1)
    * Installed basic firebase
    
    * Added build command
    
    * Better README
    
    * Close
    
    * Added SW webpack plugin
    
    * Better README
    
    * update files for firebase
    
    * Firebase setup in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import '@/assets/stylesheets/index.sass';
+import runtime from 'serviceworker-webpack-plugin/lib/runtime';
+import { fromEvent } from 'rxjs';
+
+// Firebase setup
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyDPyYczw1sWppmj_MNqQAyROTgydtd1aN4',
+  authDomain: 'pwa-twitter-4ddd4.firebaseapp.com',
+  databaseURL: 'https://pwa-twitter-4ddd4.firebaseio.com',
+  storageBucket: 'pwa-twitter-4ddd4.appspot.com',
+});
+
+// Service Worker setup
+if ('serviceWorker' in navigator) {
+  const registration = runtime.register();
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,90 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 
 module.exports = {
   mode: 'development',
 
-  devtool: 'inline-source-map',
-
   entry: {
-    app: path.resolve(__dirname, 'src', 'index.js'),
+    bundle: path.resolve(__dirname, 'src', 'index.js'),
   },
 
   output: {
     path: path.resolve(__dirname, 'build'),
-    publicPath: 'http://localhost:8080/',
   },
 
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+    new ServiceWorkerWebpackPlugin({
+      entry: path.join(__dirname, 'src', 'worker.js'),
+    }),
+    new WebpackPwaManifest({
+      name: 'My Progressive Web App',
+      short_name: 'MyPWA',
+      description: 'My awesome Progressive Web App!',
+      background_color: '#ffffff',
+      crossorigin: 'use-credentials',
+      theme_color: 'red',
+      icons: [
+        {
+          src: path.resolve(
+            __dirname,
+            'src',
+            'assets',
+            'images',
+            'logo-48.png'
+          ),
+          sizes: [96, 128, 192, 256, 384, 512],
+        },
+      ],
+    }),
+  ],
+
+  devtool: 'inline-source-map',
+
   devServer: {
-    contentBase: path.join(__dirname, 'src'),
+    contentBase: path.join(__dirname, 'public'),
     headers: { 'Access-Control-Allow-Origin': 'http://localhost:8080' },
     port: 8080,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(gif|jpg|png|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: 'assets/images',
+            },
+          },
+        ],
+      },
+
+      {
+        test: /\.sass$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
 
   resolve: {
@@ -25,26 +92,4 @@ module.exports = {
       '@': path.resolve(__dirname, 'src/'),
     },
   },
-
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-      },
-
-      {
-        test: /\.sass$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-
-  watch: true,
 };
