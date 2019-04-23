8f42b11496db0f7c4f80f8f9a60a4e05763be22c
Rodrigo Cortes <racortes2@uc.cl>
2019-04-23 13:37:40 -0400

    minor changes

--- src/connect.js
+++ src/connect.js
@@ -11,27 +11,12 @@ const pushTweet = body => {
 
   const notifications = {
     notification: {
-      title : "Hola sebi",
-      body : "saludos!",
-      click_action: "https://pwa-twitter-4ddd4.firebaseapp.com/",
+      title : "Notification",
+      body,
       icon: "https://atlas-content-cdn.pixelsquid.com/stock-images/pointer-computer-icon-B5mDxM2-600.jpg"
     },
     "to" : "dHqTyeIFZKQ:APA91bEKygiNIXkGzKlWcvRJw2PKW5eCLOrlOo6M1mj9RrtZYl72vsbOtNmviJ1vEjv41cV6C3SUQ0EVEW6_x4sa8iLYjbKpk85rK4o4q6Y4jrT-hz3R9DOZQCCpTxHspk4yzGf-SIe1"
   }
-
-  // axios({
-  //   method: 'post',
-  //   url: 'https://fcm.googleapis.com/fcm/send',
-  //   data: JSON.stringify(notifications),
-  //   headers: {
-  //     'Authorization': 'key=AAAAwCk0ixQ:APA91bEm5zrDh3CE9MLXP5QcBBUxpFL3ZPuKTo6QB2fAMSyRjfrBAJJZQ9gjws4PJGmCf_QFAsoMtH_LJruSBeLbiIxbtDLvj3ujb9QSAYRqeTFPyEUB767Hdn3bvReUSQ1xirKGvnhY',
-  //     'Content-Type': 'application/json'
-  //   }
-  // }).then(
-  //   console.log("notificacion")
-  // ).catch(err => console.log(err));
-
-  // console.log("despues de notificar");
 
   axios.post('https://fcm.googleapis.com/fcm/send',
     notifications, {
